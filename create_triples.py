973fb2c4404d97dacda8d4b7fc73104bc8687e06
Kushagra Bisen <k.bisen@yahoo.com>
2022-03-08 10:57:30 +0100

    some fucntions added to the create triples

--- create_triples.py
+++ create_triples.py
@@ -1,64 +1,127 @@
-from logging import exception
-from venv import create
+import csv
+import configparser
+from unittest import result
 import pywikibot
-import pandas as pd
 from SPARQLWrapper import SPARQLWrapper, JSON
 from util.util import WikibaseImporter
-import configparser
-import ntpath as nt
-import csv
 
 config = configparser.ConfigParser()
 config.read('config/application.config.ini')
+
 wikibase = pywikibot.Site("my", "my")
 sparql = SPARQLWrapper(config.get('wikibase', 'sparqlEndPoint'))
 site = pywikibot.Site()
+
 wikidata = pywikibot.Site("wikidata", "wikidata")
-fileXLSX = "data/BIPOC/(1977) The Combahee River Collective Statement.pdf .xlsx"
-fileCSV = "data/BIPOC/(1977) The Combahee River Collective Statement.pdf .csv"
+
 
 class CreateTriples:
     def __init__(self, wikibase, wikidata, sparql):
         self.sparql = sparql
         self.wikibase = wikibase
         self.wikibase_repo = wikibase.data_repository()
-        self.wikibase_importer = WikibaseImporter(self.wikibase_repo, self.wikidata_repo)
-
-
-    def createItem(self, label, description, key, entity_list):
-        if (self.capitaliseFirstLetter(key.rstrip()) in entity_list):
-            return entity_list
-        entity = self.getWikiItemSparql(
-            self.capitaliseFirstLetter(key.rstrip())
+        self.wikidata_repo = wikibase.data_repository()
+        self.wikibase_importer = WikibaseImporter(
+            self.wikibase_repo, self.wikidata_repo
         )
-        isExistAPI = self.getWikiItemSparql()
 
+    def searchWikiItem(self, label):
+        if label is None:
+            return True
+        params = {'action': 'wbsearchentities', 'format': 'json',
+                  'language': 'en', 'type': 'item', 'limit': 1}
+        request = self.wikibase._simple_request(**params)
+        result = request.submit()
+        print(result)
+        return True if len(result['search'] > 0) else False
 
-    def process_files(self, file_url):
-        with open(fileCSV) as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter= ',')
-            line_count = 0
+    def searchWikiItemSparql(self, label):
+        query = """
+            select ?label ?s where {
+                ?s ?p ?o.
+                ?s rdfs:label ?label.
+                FILTER(lang(?label)='fr') || label(?label) = 'en')
+                FILTER(?label = '""" + label + """'@en)
+            }
+        """
 
-            for row in csv_reader:
-                if (line_count == 0):
-                    line_count = line_count + 1
-                if(row[1] == None or row[2] == None or row[3] == None):
-                    line_count = line_count + 1
-                    continue
-                else:
-                    try:
-                        subject = nt.basename(csv_file)
+        self.sparql.setQuery(query)
+        self.sparql.setReturnFormat(JSON)
+        results = self.sparql.query().convert()
+        print(results)
+        if (len(results['results']['bindings']) > 0):
+            return True
+        else:
+            return False
 
+    wikidata_code_property_id = None
 
-                    except Exception as exception:
-                        print(exception)
+    def getWikiDataItentifier(self):
+        query = """
+        
+            select ?wikicode
+            {
+                ?wikicode rdfs:label ?plabel
+                FILTER(?plabel = 'Wikidata QID'@en)
+                FILTER(lang(?plabel) = 'fr' || lang(?plabel) = 'en')
+            }
+            limit 1
+        """
 
-def main():
-    createTriples = CreateTriples(wikibase, wikidata, sparql)
-    file_nonCSV = pd.read_excel(fileXLSX)
-    file_nonCSV.to_csv(fileCSV)
-    createTriples.process_files(fileCSV)
+        self.sparql.setQuery(query)
+        self.sparql.setReturnFormat(JSON)
+        results = self.sparql.query().convert()
 
+        if(len(results['results']['bindings']) > 0):
+            self.wikidata_code_property_id = results['results']['bindings'][0]['wikicode']['value'].split(
+                "/"
+            )[-1]
 
-if __name__ == "__main__":
-    main()
+        return results
+
+    def getItemIdByWikidataQID(self, qid):
+        query = """"
+        
+            select ?s ?label
+            where {
+                ?s ?p ?o;
+                rdfs:label ?label;
+                wdt:""" + self.wikidata_code_property_id + """  ' """ + qid + """ '.
+            }
+        """
+
+        self.sparql.setQuery(query)
+        self.sparql.setReturnFormat(JSON)
+        results = self.sparql.query().convert()
+
+        if (len(results['results']['bindings']) > 0):
+            return results['results']['bindings'][0]['s']['value'].split("/")[-1]
+        else:
+            return None
+
+    def getWikiItemSparql(self, label):
+        query = ""
+        if (self.wikidata_code_property_id is not None):
+            query = """ 
+                select ?label ?s where 
+                {
+                    ?s ?p ?o.
+                    ?s rdfs:label ?label
+                    FILTER NOT EXISTS {?s wdt:""" + self.wikidata_code_property_id + """ []}
+                    FILTER (lang(?label)='fr' || lang(?label) = 'en')
+                    FILTER(?label = '""" + label + """ '@en)
+                }
+            """
+
+        else:
+            query = """ 
+                select ?label ?s where {
+                    ?s ?p ?o.
+                    ?s rdfs:label ?label .
+                    FILTER(lang(?label) = 'fr') || lang(?label)= 'en')
+                    FILTER(?label = '""" + label + """'@en)
+                }
+            """
+
+        self.sparql.setQuery(query)
+        self.sparql.setReturnFormat(JSON)
