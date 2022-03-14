0c7e6f09869af3b104ae7b744c4b37ec9cce9f33
Kushagra Singh Bisen <k.bisen@yahoo.com>
2022-03-14 14:54:05 +0100

    added the paragraph entity

--- test.py
+++ test.py
@@ -1,3 +1,22 @@
-from pathlib import Path
-path = Path("data/Black-Disability/CSV/(1977) The Combahee River Collective Statement.csv")
-print(path.parent.absolute())
+import csv
+
+filePath = "data/Black-Disability/CSV/(1977) The Combahee River Collective Statement.csv"
+
+with open(filePath, 'r') as csv_file:
+    csv_reader = csv.DictReader(csv_file, delimiter = ',')
+    line_count = 0
+    for line in csv_reader:
+        print(f'currently on the line {line_count}')
+        try:
+            paragraph_text = line['Paragraph']
+            paragraph_labels = []
+
+            for i in range(1, 15):
+                if(line[f'Label {i}']) != "":
+                    paragraph_labels.append(line[f'Label {i}'].capitalize())
+            print(paragraph_labels)
+
+
+        except Exception as e:
+            print('The exception encountered is ', e)
+        line_count = line_count + 1
