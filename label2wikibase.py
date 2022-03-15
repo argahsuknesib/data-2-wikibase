a3fab0836f1b9b0cf8116477f2e4fbbd3a035db5
Kushagra Singh Bisen <k.bisen@yahoo.com>
2022-03-15 14:31:09 +0100

    changed from dict to list

--- label2wikibase.py
+++ label2wikibase.py
@@ -315,7 +315,7 @@ class UploadLabels():
                             paragraph_topics.append(line[f'Label {i}'].capitalize())
                         else:
                             pass
-                    paragraph_subtopics = {language_code : paragraph_topics}
+                    paragraph_subtopics = paragraph_topics
 
                     paragraph_entity = self.createParagraphEntity(label = paragraph_label, description = paragraph_description, text = paragraph_text, document_entity= wiki_doc_item , sub_topics= paragraph_subtopics, lang = language_code)
                     # paragraph_entity.get()
