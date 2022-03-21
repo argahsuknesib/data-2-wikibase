eca690e272fb2d5f4f61ebb9859ff58e5f2fb326
Kushagra Singh Bisen <k.bisen@yahoo.com>
2022-03-21 10:49:00 +0100

    checked the strip method

--- test.py
+++ test.py
@@ -1,2 +1,14 @@
-dict = "1 2 3 "
-print(dict.label)
+import csv
+
+with open('data/Black-Disability/CSV/Black Disability Gone Viral word.csv') as csv_file:
+    csv_reader = csv.DictReader(csv_file, delimiter = ',')
+    line_count = 0
+    for line in csv_reader:
+        print(f'currently on the line {line_count}')
+        try:
+            paragraph_text = line['Paragraph'].strip()
+            print(paragraph_text)
+        except Exception as e:
+            print(e)
+
+        line_count = line_count + 1
