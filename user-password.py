974ffee5f0900d92acd408b1ce86451321ea5ccc
Kushagra Singh Bisen <k.bisen@yahoo.com>
2022-03-21 12:06:38 +0100

    changed password for the bot

--- user-password.py
+++ user-password.py
@@ -1,4 +1,5 @@
 # (u'WikibaseAdmin', BotPassword(u'bot_user_1', u'rgd9gj5jpaflqemnkh8k0vcl3qmde425'))
 #(u'WikibaseAdmin', BotPassword(u'Disability-Wikibase', u'j573bokmv4cui3tldafvj3pu4jsqchg7'))
 # (u'WikibaseAdmin', BotPassword(u'Disability-Wikibase', u'vt73k6luh8rpvf9drqb729kf5sap75k7'))
-(u'WikibaseAdmin', BotPassword(u'Disability-Wikibase', u'jbnjr2hifd19q5cp89cqo2chp6l6ih7l'))
+#(u'WikibaseAdmin', BotPassword(u'Disability-Wikibase', u'jbnjr2hifd19q5cp89cqo2chp6l6ih7l'))
+(u'WikibaseAdmin', BotPassword(u'Disability-Wikibase', u'89g8vocsc04n5sdqr96uoc84uon65728'))
